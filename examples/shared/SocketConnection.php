1ecd825a9679111fb2e4d1389cd3c993caa29fa6
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2024-11-26 21:49:52 +0000

    Implemented socket connection timeout in examples.

--- examples/shared/SocketConnection.php
+++ examples/shared/SocketConnection.php
@@ -6,6 +6,7 @@ use Amp\DeferredCancellation;
 use Amp\Socket\Socket;
 
 use AaronicSubstances\Kabomu\Abstractions\DefaultQuasiHttpProcessingOptions;
+use AaronicSubstances\Kabomu\Abstractions\DefaultTimeoutResult;
 use AaronicSubstances\Kabomu\Abstractions\QuasiHttpConnection;
 use AaronicSubstances\Kabomu\Abstractions\QuasiHttpProcessingOptions;
 use AaronicSubstances\Kabomu\Abstractions\QuasiHttpResponse;
@@ -16,6 +17,7 @@ class SocketConnection implements QuasiHttpConnection {
     private readonly ?DeferredCancellation $connectCancellation;
     private readonly QuasiHttpProcessingOptions $processingOptions;
     private readonly ?\Closure $timeoutScheduler;
+    private readonly ?DeferredCancellation $timeoutCancellation;
 
     private ?array $environment = null;
 
@@ -33,11 +35,44 @@ class SocketConnection implements QuasiHttpConnection {
         }
         $this->processingOptions = $effectiveProcessingOptions;
 
-        $this->timeoutScheduler = null;
+        $timeoutMillis = $effectiveProcessingOptions->getTimeoutMillis();
+        if ($timeoutMillis <= 0) {
+            $this->timeoutScheduler = null;
+            $this->timeoutCancellation = null;
+        }
+        else {
+            $this->timeoutCancellation = new DeferredCancellation;
+            $this->timeoutScheduler = function($proc) use($timeoutMillis) {
+                $task1 = \Amp\async(function() use($timeoutMillis) {
+                    try {
+                        \Amp\delay($timeoutMillis / 1_000, true, $this->timeoutCancellation->getCancellation());
+                        $timeoutResult = new DefaultTimeoutResult(true, null, null);
+                    }
+                    catch (CancelledException $ignore) {
+                        $timeoutResult = new DefaultTimeoutResult(false, null, $ignore);
+                    }
+                    return $timeoutResult;
+                });
+                $task2 = \Amp\async(function() use($proc) {
+                    try {
+                        $res = $proc();
+                        $runResult = new DefaultTimeoutResult(false, $res, null);
+                    }
+                    catch (\Throwable $e) {
+                        $runResult = new DefaultTimeoutResult(false, null, $e);
+                    }
+                    return $runResult;
+                });
+                $res = \Amp\Future\awaitFirst([$task1, $task2]);
+                $this->timeoutCancellation->cancel();
+                return $res;
+            };
+        }
     }
 
     public function release(?QuasiHttpResponse $response) {
         $this->connectCancellation?->cancel();
+        $this->timeoutCancellation?->cancel();
         if ($response?->getBody()) {
             return;
         }
